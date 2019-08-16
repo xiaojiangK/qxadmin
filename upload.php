7499bfbfb0b7dbcb92854e10627c3d2ad92be81b
xiaojiangK <740092856@qq.com>
2019-08-16 13:58:44 +0800

    完成表单提交返回数据&login check

--- upload.php
+++ upload.php
@@ -1,4 +1,12 @@
 <?php
     header('Content-Type: text/html; charset=utf-8');
-    var_dump($_FILES);
+    if ($_FILES['file']) {
+        var_dump($_FILES);
+    }
+    if (count($_GET)) {
+        var_dump($_GET);
+    }
+    if (count($_POST)) {
+        var_dump($_POST);
+    }
 ?>
